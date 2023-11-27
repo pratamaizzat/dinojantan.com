28753ff9cc81c0cf91b4c3d157869ab2f2abcf85
pratamaizzat <pratama.izzat231298@gmail.com>
2023-11-27 22:38:38 +0700

    add retha theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,31 @@ export default {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['light', 'dark', 'cupcake'],
+    themes: [
+      'light',
+      'dark',
+      'retro',
+      {
+        retha: {
+          primary: '#ff00ff',
+
+          secondary: '#00dcff',
+
+          accent: '#af7c00',
+
+          neutral: '#1f1d12',
+
+          'base-100': '#392623',
+
+          info: '#008199',
+
+          success: '#9dee00',
+
+          warning: '#ffad00',
+
+          error: '#e0003a',
+        },
+      },
+    ],
   },
 }
