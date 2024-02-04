1f67174078dfaa1d29dccaba2d5e7b0bc346bf3c
pratamaizzat <pratama.izzat231298@gmail.com>
2024-02-04 16:19:50 +0700

    change to shadcn

---
+++ src/configs/social.ts
@@ -0,0 +1,22 @@
+export const socials = [
+  {
+    name: 'Tiktok',
+    href: 'https://www.tiktok.com/@dino.gank.code',
+    icon: 'line-md:tiktok',
+  },
+  {
+    name: 'Twitter',
+    href: 'https://twitter.com/dinosaurjantan',
+    icon: 'carbon:logo-twitter',
+  },
+  {
+    name: 'Instagram',
+    href: 'https://www.instagram.com/dino.gank.code',
+    icon: 'line-md:instagram',
+  },
+  {
+    name: 'Linkedin',
+    href: 'https://www.linkedin.com/in/pratamaizzat/',
+    icon: 'line-md:linkedin',
+  },
+]
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,36 +1,72 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['class'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  prefix: '',
   theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: [
-      'light',
-      'dark',
-      'retro',
-      {
-        retha: {
-          primary: '#ff00ff',
-
-          secondary: '#00dcff',
-
-          accent: '#af7c00',
-
-          neutral: '#1f1d12',
-
-          'base-100': '#392623',
-
-          info: '#008199',
-
-          success: '#9dee00',
-
-          warning: '#ffad00',
-
-          error: '#e0003a',
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
         },
       },
-    ],
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
+  plugins: [require('tailwindcss-animate')],
 }
