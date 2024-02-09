966e26a8145292599994e66b68b6ce4c78c18fa8
pratamaizzat <pratama.izzat231298@gmail.com>
2024-02-09 23:25:41 +0700

    add refrence blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
-
+import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.dinojantan.com',
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     icon(),
     react(),
+    sitemap(),
   ],
 })
